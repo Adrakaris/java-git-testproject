0d2053899ad59b52f5c4ac221bfed9111a176638
Yijun Hu <huyijun007@gmail.com>
2020-04-13 13:54:30 +0100

    Maze solver solves from file now; Will print correct solution

--- MazeSolver/src/mazesolver/Maze.java
+++ MazeSolver/src/mazesolver/Maze.java
@@ -5,6 +5,6 @@ import java.util.LinkedList;
 public class Maze {
     public int[][] maze;  // maze thing
     // 0 wall 1 path 2 dest
-    public LinkedList<Position> path;  // path taken
+    public LinkedList<Position> path = new LinkedList<>();  // path taken
     public Position start;  // start pos
 }
--- MazeSolver/src/mazesolver/Solver.java
+++ MazeSolver/src/mazesolver/Solver.java
@@ -1,58 +1,34 @@
 package mazesolver;
 
-import java.lang.reflect.Array;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Solver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // creating list of mazes
-        ArrayList<Maze> mazes = new ArrayList<>();
-
-        // Creating mazes
-        // 0 wall 1 path 2 dest
-        Maze m = new Maze();
-        m.maze = new int[][]{
-            {1,1,1,1,0,1,1,1,0,1,0},
-            {0,0,1,1,1,1,0,0,0,1,0},
-            {0,0,0,1,0,1,1,1,1,1,1},
-            {1,1,1,2,0,1,0,1,0,1,0},
-            {0,0,1,0,1,0,0,0,1,1,0},
-            {0,0,0,1,1,1,1,0,0,0,0}
-        };
-        m.start = new Position(4, 8);
-        m.path = new LinkedList<>();
-
-        Maze n = new Maze();
-        n.maze = new int[][]{
-            {2,1,1,1,0,1,1,1,0,1,0},
-            {0,0,1,1,1,1,0,0,0,1,0},
-            {0,0,0,1,0,1,1,1,1,1,1},
-            {1,1,1,1,1,1,0,1,0,1,0},
-            {0,0,1,0,1,0,0,0,1,1,0},
-            {0,0,0,1,1,1,1,0,0,0,0}
-        };
-        n.start = new Position(0, 9);
-        n.path = new LinkedList<>();
-
-        //adding mazes
-        mazes.add(m);
-        mazes.add(n);
+        ArrayList<Maze> mazes = readMazes(); // new ArrayList<>();
 
         // looping over all mazes
         for (Maze i : mazes) {  // fancy ass for each
             boolean result = solveMaze(i);
+            System.out.println();
             if (result) {
-                System.out.println("Win! Explored paths:");
+                System.out.println("Win! Path (o):");
 
                 // display le display
                 for (int[] j : i.maze) {  // for every row
                     for (int k : j) {  // print dep
                         if (k == -1) {  // should be path
                             System.out.print("o");
+                        } else if (k == 69) {  // any explored that was dead end
+                            System.out.print("_");
                         } else if (k == 1) {  // should be free
                             System.out.print("_");
+                        } else if (k == 2) {
+                            System.out.print("*");
                         } else {  // anything else is a wall
                             System.out.print("#");
                         }
@@ -66,6 +42,65 @@ public class Solver {
         }
     }
 
+    /**
+     * Read any number of mazes from an input file mazes.txt
+     * <p>
+     *     This method uses the file <code>mazes.txt</code> to read a list of mazes from. Mazes should be formatted
+     *     accordingly, as with the given example:
+     * </p>
+     * <p> <code>
+     *     6 | The number of rows the maze has
+     *     1,1,1,1,0,1,1,1,0,1,0 | the maze itself, which
+     *     0,0,1,1,1,1,0,0,0,1,0 | is written according to
+     *     0,0,0,1,0,1,1,1,1,1,1 | the following key:
+     *     1,1,1,2,0,1,0,1,0,1,0 | 0: wall
+     *     0,0,1,0,1,0,0,0,1,1,0 | 1: path
+     *     0,0,0,1,1,1,1,0,0,0,0 | 2: goal
+     *     4 | The y-coord (row index) of the start position
+     *     8 | The x-coord (col index) of the start position
+     *     --------------------- Divider, can be anything </code>
+     * </p>
+     *
+     * @return Array of stored mazes
+     * @throws FileNotFoundException
+     */
+    private static ArrayList<Maze> readMazes() throws FileNotFoundException {
+        ArrayList<Maze> mazes = new ArrayList<>();
+        Scanner in = new Scanner(new File("mazes.txt"));
+
+        while (in.hasNext()) {
+            // Filling list from file
+            // 0 wall 1 path 2 dest
+            // for n rows
+            Maze m = new Maze();
+            int nRows = Integer.parseInt(in.nextLine());  // gets the number of rows
+            m.maze = new int[nRows][];
+
+            // loop to incr
+            for (int z = 0; z < nRows; z++) {
+                // gets the row of the maze file, and reads it to an int array
+                m.maze[z] = Arrays.stream(in.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+            }
+
+            // for positions
+            int y = Integer.parseInt(in.nextLine());
+            int x = Integer.parseInt(in.nextLine());
+            m.start = new Position(y, x);
+            mazes.add(m);
+
+            in.nextLine();  // reads the blank line and discard it
+        }
+
+        in.close();
+        return mazes;
+    }
+
+    /**
+     * Solves a maze m.
+     *
+     * @param m Object of type Maze
+     * @return True for solveable, false for unsolvable
+     */
     public static boolean solveMaze(Maze m) {
         m.path.push(m.start);  // must be static to do this and access it w/o creating Solver class
 
@@ -81,9 +116,10 @@ public class Solver {
             if (isValid(m, y, x)) {
                 if (m.maze[y][x] == 2) {
                     return true;
+                } else {
+                    m.maze[y][x] = -1;  // set start to -1
                 }
             }
-            m.maze[y][x] = -1;  // set start to -1
             // System.out.println(Arrays.deepToString(m.maze));
 
             // south
@@ -104,8 +140,9 @@ public class Solver {
             }
 
             // to backtrack
-            System.out.println("Dead end, backtracking.");
-            m.path.pop();
+            System.out.print("Backtracking. ");
+            Position dd = m.path.pop();
+            m.maze[dd.y][dd.x] = 69;
             if (m.path.size() <= 0) {
                 return false;
             }
@@ -113,6 +150,20 @@ public class Solver {
 
     }
 
+    /**
+     * Makes a move on the maze if a valid move is to be found.
+     * <p>
+     *     Method will try to move to the coordinates specified, should they be within the array, and return whether
+     *     it is possible or not. A direction should be provided as the method will print to console the direction
+     *     of movement. For example, for a movement south, "South" should be passed.
+     * </p>
+     *
+     * @param m Object of type Maze
+     * @param y Index of row
+     * @param x Index of column
+     * @param dir Direction
+     * @return True for valid movement, else false
+     */
     public static boolean move(Maze m, int y, int x, String dir) {
         // move if the path is valid
         // win should already be checked
@@ -123,13 +174,21 @@ public class Solver {
                 m.maze[y][x] == 2) {
                 // System.out.println("'Tis a one.");
                 m.path.push(new Position(y, x));
-                System.out.printf("Moved %s.%n", dir);
+                System.out.printf("%s. ", dir);
                 return true;
             }
         }
         return false;
     }
 
+    /**
+     * Checks if the coordinates are within the bounds of the array.
+     *
+     * @param m Object of type Maze
+     * @param y Index of row
+     * @param x Index of column
+     * @return True for valid, false for invalid
+     */
     public static boolean isValid(Maze m, int y, int x) {
         /* if (y < 0 ||
                 y >= m.maze.length ||
